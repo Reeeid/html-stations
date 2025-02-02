81dc59e06b1a8b4d491dbcf65b31e543fd28d195
BuildTools <unconfigured@null.spigotmc.org>
2025-02-02 18:09:44 +0900

    finish

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -1,16 +1,38 @@
+const { first } = require("fp-ts/lib/Semigroup");
+const { resolveModuleName } = require("typescript");
+
 async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function test(userList) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const updatedList = userList.map((user) => ({
+        ...user,
+        full_name: user.family_name + " " + user.first_name,
+      }));
+      resolve(updatedList);
+    }, 3000);
+  });
 }
-
